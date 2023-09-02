4849b2754b1a9cc06e8f50d90925557933bf8c40
lohan2015 <mbacyrille2002@yahoo.fr>
2023-09-02 07:30:40 -0400

    Modification service modification fiche salarié

--- src/main/java/com/mcsoftware/amplituderh/repository/SalarieRepository.java
+++ src/main/java/com/mcsoftware/amplituderh/repository/SalarieRepository.java
@@ -3,6 +3,9 @@ package com.mcsoftware.amplituderh.repository;
 import com.mcsoftware.amplituderh.entity.Rhpagent;
 import com.mcsoftware.amplituderh.entity.RhpagentId;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface SalarieRepository extends JpaRepository<Rhpagent, RhpagentId>  {
 
